12459977beadf6d57b30393928796b5549e54412
Muzahid <mdmuzahid.gd@gmail.com>
2023-05-19 09:53:27 +0600

    full transection calculation add succesfully

--- Transection.js
+++ Transection.js
@@ -18,3 +18,27 @@ document.getElementById("DepositeButton").addEventListener("click", function(){
 
     DepositeField.value = "";
 })
+
+    // withdraw balance section
+document.getElementById("WithdrawButton").addEventListener("click", function(){
+    const Withdrawfield = document.getElementById("WithdrawMoney")
+    const WithdrawMoney = parseFloat(Withdrawfield.value);
+
+    const PreWithdraw = document.getElementById("Withdraw");
+    const Withdraw = parseFloat(PreWithdraw.innerText);
+
+    const Totalwithdraw = Withdraw + WithdrawMoney;
+    PreWithdraw.innerText = Totalwithdraw;
+
+    const Totalbalance = document.getElementById("TotalBalance");
+    const TotalBalance = parseFloat(Totalbalance.innerText);
+    const NewTotalBalance = TotalBalance - WithdrawMoney;
+    Totalbalance.innerText = NewTotalBalance;
+
+    if(TotalBalance <= WithdrawMoney){
+        alert("You have no sufficient balance. Please Deposite Some money.")
+    }
+
+    Withdrawfield.value = "";
+    
+})
